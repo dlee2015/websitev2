c75f5d3e44da450b46db89d323c34ce901c69446
dlee2015 <dlee@Daniels-MacBook-Air.local>
2019-05-17 19:57:56 -0700

    add mongo atlas

--- app.js
+++ app.js
@@ -1,24 +1,40 @@
 const express = require('express'),
-      app = express(),
-      bodyParser = require('body-parser');
+	app = express(),
+	bodyParser = require('body-parser'),
+	mongoose = require('mongoose'),
+	Projects = require('./models/Projects');
 
-app.use(express.static("public"));
-app.set("view engine", "ejs");
+const db = require('./config/keys').mongoURI;
 
+mongoose
+	.connect(db, { useNewUrlParser: true })
+	.then(() => console.log('DB Has connected'))
+	.catch(err => console.log(err));
 
-app.get("/", function(req, res){
-    res.render("index");
-})
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
 
-app.get('/about', function(req, res){
-    res.render('about');
-})
+const newProject = {
+	Title: 'Hello World',
+	About: 'Bello World'
+};
 
-app.get('/projects', function(req, res){
-    res.render('projects');
-})
+Projects.create(newProject, err => {
+	if (err) {
+		console.log(err);
+	}
+});
 
+app.get('/', function(req, res) {
+	res.render('index');
+});
 
+app.get('/about', function(req, res) {
+	res.render('about');
+});
 
+app.get('/projects', function(req, res) {
+	res.render('projects');
+});
 
-app.listen(process.env.PORT  || 4000);
+app.listen(process.env.PORT || 4000);
---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+	mongoURI:
+		'mongodb+srv://dlee2015:w7pePqoH8DBjIQuy@project-list-6qrew.mongodb.net/test?retryWrites=true'
+};
